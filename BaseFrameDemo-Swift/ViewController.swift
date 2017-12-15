a7f5935e702f9de038dc01a810df03faf029dc5f
贾晶晶 <jiajingjing@1jia2.cn>
2017-12-15 14:04:35 +0800

    Initial Commit

---
+++ BaseFrameDemo-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BaseFrameDemo-Swift
+//
+//  Created by Jimmy on 2017/12/15.
+//  Copyright © 2017年 Jimmy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
