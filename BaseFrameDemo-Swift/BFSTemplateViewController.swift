ea8e957e8f28c88671db434de7ab3ba897601224
jingjing jia <jiajingjing@lifang.com>
2017-12-19 21:03:22 +0800

    update library

--- BaseFrameDemo-Swift/BFSTemplateViewController.swift
+++ BaseFrameDemo-Swift/BFSTemplateViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class BFSTemplateViewController: UIViewController {
-
+    
+    //MARK:常量和变量
+    
+    let maximumNumberOfLogicAttemps = 10
+    var currentLoginAttempt = 0
+    var deviceModels: [String] = []
+    var faxNumber: Int?
+    
+    
+    // 这是一个注释
+    /* 这是一个, 多行注释 */
+    /* 这是第一个多行注释的开头
+    /* 这是第二个被嵌套的多行注释 */ 这是第一个多行注释的结尾 */
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +33,15 @@ class BFSTemplateViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    //MARK:函数
 
+    //func sayHelloWorld() -> String {  //没有参数
+    //func sayHelloWorld(person: String) -> Void {  //没有返回值
+
+    func sayHelloWorld(person: String) -> String {
+        return "Hollo World";
+    }
+    //print(greetAgain(person: "Anna"))
     /*
     // MARK: - Navigation
 
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios,’10.0’
+
+target ‘BaseFrameDemo-Swift’ do
+pod 'RxSwift',    '~> 4.0'
+pod 'RxCocoa',    '~> 4.0'
+pod 'Alamofire'
+end
